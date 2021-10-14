075c7ce4f66d6b085b486bcb677a0f7a4574adf7
Luca Previtali <lucaprevi@gmail.com>
2021-10-14 17:40:43 +0200

    resize avatar image and get random 404 image

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import jsonFetch from 'simple-json-fetch'
 import styled from 'styled-components'
-import { withPrefix } from 'gatsby'
 import loadable from '@loadable/component'
 import Wrapper from '../components/wrapper'
 import SEO from '../components/SEO'
 
 const Layout = loadable(() => import('../components/layout'))
+
+const endpoint = `https://random-404-api.herokuapp.com/random-image`
 
 const MainTitle = styled.h1`
   line-height: 1.5;
@@ -13,9 +15,11 @@ const MainTitle = styled.h1`
   font-size: 3rem;
 `
 
-const Icon = styled.div`
-  width: 150px;
-  margin: 0 auto;
+const Image = styled.div`
+  img {
+    width: 90%;
+    height: 600px;
+  }
 `
 
 const Text = styled.p`
@@ -31,25 +35,39 @@ const TurnHome = styled.div`
   }
 `
 
-const NotFoundPage = ({ location }) => (
-  <>
-    <SEO title="Page Not Found" />
-    <Layout location={location.pathname} noCover={true}>
-      <Wrapper>
-        <MainTitle>404 Page Not Found</MainTitle>
-        <Icon>
-          <img src={withPrefix('/images/ufo-and-cow.svg')} alt="ufo and cow" />
-        </Icon>
-        <Text>
-          Looks like you've followed a broken link or entered a URL that doesn't
-          exist on this site.
-        </Text>
-        <TurnHome>
-          <a href="/">Turn to homepage</a>
-        </TurnHome>
-      </Wrapper>
-    </Layout>
-  </>
-)
+const NotFoundPage = ({ location }) => {
+  const [image, setImage] = useState()
+
+  useEffect(() => {
+    async function fetchData() {
+      const imageNotFound = await jsonFetch(endpoint)
+      if (imageNotFound.json?.src) {
+        setImage(imageNotFound.json.src)
+      }
+    }
+    fetchData()
+  }, [setImage])
+
+  return (
+    <>
+      <SEO title="Page Not Found" />
+      <Layout location={location.pathname} noCover={true}>
+        <Wrapper>
+          <MainTitle>404 Page Not Found</MainTitle>
+          <Image>
+            <img src={image} alt="random 404" />
+          </Image>
+          <Text>
+            Looks like you've followed a broken link or entered a URL that
+            doesn't exist on this site.
+          </Text>
+          <TurnHome>
+            <a href="/">Turn to homepage</a>
+          </TurnHome>
+        </Wrapper>
+      </Layout>
+    </>
+  )
+}
 
 export default NotFoundPage
