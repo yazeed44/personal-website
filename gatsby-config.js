07229d1595d89b4e4242f24e55b6aaa03de8fb4d
Yazeed Almuqwishi <yazeed.almuqwishi@gmail.com>
2022-06-30 14:51:47 +0000

    no need for tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,9 +31,6 @@ module.exports = {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: [
-          config.googleAnalyticsId, // Google Analytics / GA
-        ],
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
